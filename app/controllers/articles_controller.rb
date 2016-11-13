c4f8fc6ee77317566547cfe96305a4c297bfd660
Derrick Carr <derrick.the.future@gmail.com>
2016-11-13 11:11:54 -0800

    Add basic rails auth for articles andd commetn deletion.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "derrick", password: "liberatory", except: [:index, :show] 
   def index
     @articles = Article.all
   end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  http_basic_authenticate_with name: "derrick", password: "liberatory", only: :destroy 
 
   def create
     @article = Article.find(params[:article_id])
